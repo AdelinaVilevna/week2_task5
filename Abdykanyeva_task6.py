0f3370b4a99729ed612d915a416f9f9d894205d8
AdelinaVilevna <adelina.abdykanyeva@gmail.com>
2020-11-02 23:30:17 +0600

    week2_task5

---
+++ Abdykanyeva_task6.py
@@ -0,0 +1,43 @@
+# Question 1
+
+# a = {1, 6, 4, 0, 34, 17, 100, -45}
+# b = {22, 0, 3, 100, 4}
+# x = a & b
+# print(x)
+# x = a - b
+# print(x)
+# x = a | b
+# print(x)
+
+# Question 2
+# Aibek = {"ImperiaPizza", "Dodo", "Freshbox"}
+# Aidai = {"OchakKebab", "Freshbox"}
+# Azat = {"Freshbox", "KFC"}
+# Gulzana = {"Dodo", "Freshbox", "ImperiaPizza", "OchakKebab", "KFC"}
+# print( Aibek & Aidai & Azat & Gulzana)
+
+
+# Question 3
+# Djon = {5, 7, 11, 10, 28}
+# Reichel = {29, 2, 6, 12, 3}
+# Ketrin = {1, 5, 14, 8, 22}
+# Sem = {19, 20, 3, 11, 10}
+# print(Djon & Reichel & Ketrin & Sem)
+
+# Question 4
+# ingredients = {"syr cheddar", "griby", "sous", "shpinat"}
+# ingredients.add("pomidory")
+# print(ingredients)
+# if "sausage" in ingredients:
+#     ingredients.remove("sausage")
+# print(ingredients)
+# if "shpinat" in ingredients:
+#     ingredients.remove("shpinat")
+# print(ingredients)
+# ingredients.add("bazilik")
+# print(ingredients)
+# ingredients.remove("syr cheddar")
+# ingredients.add("mozarella")
+# print(ingredients)
+
+# Poluchilos' pizza Margarita
